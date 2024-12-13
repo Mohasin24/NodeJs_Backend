0cfc6b44bcd7c0dd4d70adf70feb5c1aba239b03
Mohasin Patel <mohasinpatel313@gmail.com>
2024-12-13 11:24:20 +0530

    util update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,28 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { LIMIT } from "./constant.js";
 
 const app = express();
+
+// Middlewares
+
+app.use(cors({
+     origin: process.env.CORS_ORIGIN,
+     credentials: true
+}));
+
+app.use(express.json({
+     limit: LIMIT
+}));
+
+app.use(express.urlencoded({
+     extended: true,
+     limit: LIMIT
+}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
 
 export default app
