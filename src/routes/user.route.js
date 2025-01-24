29045d02cd140e6005b4b99b182e71fe9102d2e5
Mohasin Patel <mohasinpatel313@gmail.com>
2025-01-24 13:40:47 +0530

    User controller and User route update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -4,7 +4,14 @@ import {
      deleteUserById,
      loginUser,
      logoutUser,
-     refreshAccessToken
+     refreshAccessToken,
+     changeUserCurrentPassword,
+     getCurrentLoggedInUser,
+     updateAccountDetails,
+     updateUserAvatar,
+     updateUserCoverImage,
+     getUserChannelProfile,
+     getUserWatchHistory
 } from "../controller/user.controller.js"
 import { upload } from "../middleware/multer.middleware.js"
 import { verifyJWT } from "../middleware/auth.middleware.js"
@@ -37,6 +44,18 @@ router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT,changeUserCurrentPassword)
 
+router.route("/current-user").get(verifyJWT,getCurrentLoggedInUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload,updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload,updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getUserWatchHistory)
 
 export default router;
